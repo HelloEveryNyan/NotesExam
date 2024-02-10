0c1a620751bca08834f7cff4737de06972f2254c
Era <ghostov142@gmail.com>
2024-02-11 04:15:54 +0700

    Создание класса

--- notes.py
+++ notes.py
@@ -1,3 +1,8 @@
 import json
 import os
 from datetime import datetime
+
+class NotesApp:
+    def __init__(self, notes_file='notes.json'):
+        self.notes_file = notes_file
+        self.notes = self.load_notes()
