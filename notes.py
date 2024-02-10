90c12b364e944d5d10b9f6cb90f121ea07826f42
Era <ghostov142@gmail.com>
2024-02-11 05:28:49 +0700

    Создание, сохранение, загрузка

--- notes.py
+++ notes.py
@@ -6,3 +6,22 @@ class NotesApp:
     def __init__(self, notes_file='notes.json'):
         self.notes_file = notes_file
         self.notes = self.load_notes()
+
+    def load_notes(self):
+        if os.path.exists(self.notes_file):
+            with open(self.notes_file, 'r') as file:
+                return json.load(file)
+        else:
+            return []
+
+    def save_notes(self):
+        with open(self.notes_file, 'w') as file:
+            json.dump(self.notes, file, indent=2)
+
+    def add_note(self, title, body):
+        note = {
+            'id': len(self.notes) + 1,
+            'title': title,
+            'body': body,
+            'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        }        
